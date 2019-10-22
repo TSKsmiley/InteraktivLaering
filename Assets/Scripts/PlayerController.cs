d844b2d075d9777ee2a4bf7d460c009c17c31b45
FredTheNoob <frederikhammer02@gmail.com>
2019-10-22 14:43:13 +0200

    Fixed WASD not updating axis when looking around

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,8 +21,17 @@ public class PlayerController : MonoBehaviour
     // FixedUpdate bruges til fysik-baserede handlinger, da man kalder denne update på en specifik frame.
     private void FixedUpdate()
     {
-        // Tilføj en hastighed til kameraets rigidbody. Få heraf den horisontale samt vertikale akse for at skabe WASD movement. Udelad y-aksen, da vi ikke skal opad på disse keys. Gang til sidst alt dette med vores float speed, som kan sættes i unity editoren
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+        // Få x og y-akserne og multiplicer disse med vores hastighedsvariabel. Set y-aksen til 0, da vi ikke skal opad
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+
+        //Få akserne til at følge der hvor kameraet kigger hen
+        movement = tf.TransformDirection(movement);
+
+        // Sikre y-aksen er i nul, ved at omdanne tilbage til globalt akseniveau. På denne måde kan man ikke gå opad på y-aksen
+        movement = new Vector3(movement.x, 0.0f, movement.z);
+        
+        // Få spilleren til at bevæge sig
+        rb.velocity = movement;
     }
 
     // Update kaldes en gang per frame
