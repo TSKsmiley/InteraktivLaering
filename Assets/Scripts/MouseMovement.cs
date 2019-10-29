760b0262bd939fa56b223c5549a5aec0331b4f39
FredTheNoob <frederikhammer02@gmail.com>
2019-10-29 15:12:43 +0100

    Splitted the playercontroller and mouse movement into two scripts. TODO: Make player object follow camera y-axis rotation

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public Transform tf;
+    public Transform tfPlayer;
+
+    public Rigidbody rb;
+  
+
+    public float mouseSensitity;
+
+    // Start er kaldt inden den første frame update
+    void Start()
+    {
+        // Gør cursoren usynlig for spilleren
+        Cursor.visible = false;
+        // Centrér cursoren midt i skærmen, så man starter med at have musen i midten af skærmen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update kaldes en gang per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // Rotér transformen i forhold til museposition. Set z-aksen til 0, da vi skal ordne den fra linje 40 og til linje 45
+        Vector3 rotation = new Vector3(-mouseY * mouseSensitity, mouseX * mouseSensitity, 0);
+        tf.Rotate(rotation);
+
+        tfPlayer.Rotate(new Vector3(0, rotation.y, 0));
+
+        // Med inspiration fra Michaels fysikspil
+        float z = tf.eulerAngles.z; // Skaf vinklen z
+
+        tf.Rotate(0, 0, -z); // Rotér z i minus
+
+        // Flyt z-aksen tilbage til 0
+        //rb.AddForce(0, 100 * Time.deltaTime, 0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,24 +10,13 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
-    public float mouseSensitity;
-
-    // Start er kaldt inden den første frame update
-    void Start()
-    {
-        // Gør cursoren usynlig for spilleren
-        Cursor.visible = false;
-        // Centrér cursoren midt i skærmen, så man starter med at have musen i midten af skærmen
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
     // FixedUpdate bruges til fysik-baserede handlinger, da man kalder denne update på en specifik frame.
     private void FixedUpdate()
     {
         // Få x og y-akserne og multiplicer disse med vores hastighedsvariabel. Set y-aksen til 0, da vi ikke skal opad
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
 
-        //Få akserne til at følge der hvor kameraet kigger hen
+        // Få akserne til at følge der hvor kameraet kigger hen
         movement = tf.TransformDirection(movement);
 
         // Sikre y-aksen er i nul, ved at omdanne tilbage til globalt akseniveau. På denne måde kan man ikke gå opad på y-aksen
@@ -37,23 +26,5 @@ public class PlayerController : MonoBehaviour
         rb.velocity = movement;
     }
 
-    // Update kaldes en gang per frame
-    void Update()
-    {
-        float MouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        //tf.Rotate(MouseX * mouseSensitity, mouseY * mouseSensitity, 0);
-
-        // Rotér transformen i forhold til museposition. Set z-aksen til 0, da vi skal ordne den fra linje 40 og til linje 45
-        tf.Rotate(new Vector3(-mouseY * mouseSensitity, MouseX * mouseSensitity, 0));
-
-        // Med inspiration fra Michaels fysikspil ;)
-        float z = tf.eulerAngles.z; // Skaf vinklen z
-
-        tf.Rotate(0,0, -z); // Rotér z i minus
-
-        // Flyt z-aksen tilbage til 0
-        rb.AddForce(0, 100 * Time.deltaTime, 0);
-    }
+    
 }
