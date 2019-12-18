75a632ca698588a4bb2187b256076ed1b0e3d9f3
FredTheNoob <frederikhammer02@gmail.com>
2019-12-18 07:59:14 +0100

    Small fixes
    Based on player feedback, I changed height of input pipe. Also added so that when you throw elements out of the map, they come back to the playground (performance issues prevented)

---
+++ Assets/Scripts/MoveElements.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dette script har til formål at flytte kemikalier tilbage til spilleren, hvis han kaster den udover mappet (dette forhindrer performance problemer)
+public class MoveElements : MonoBehaviour
+{
+    // Lav en reference til et punkt over gulvet
+    public Transform moveElPoint;
+    private void OnTriggerEnter(Collider col)
+    {
+        // Flyt objektet til punktet, hvis den kolliderer med den trigger som sidder under gulvet
+        col.gameObject.transform.position = moveElPoint.position;
+    }
+}
